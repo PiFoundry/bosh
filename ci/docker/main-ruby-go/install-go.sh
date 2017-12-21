04e077854e38ef053b287f0e3d6f3b5ee61a56d8
Ruurd Keizer <ruurdk@hotmail.com>
2017-12-21 15:37:58 +0100

    add todo

--- ci/docker/main-ruby-go/install-go.sh
+++ ci/docker/main-ruby-go/install-go.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 set -eux
-
+# TODO ADD THE ARM Go Binaries or build from source
 GOPATH=/home/vagrant/go
 GO_ARCHIVE_URL=https://storage.googleapis.com/golang/go1.8.3.linux-amd64.tar.gz
 GO_ARCHIVE_SHA256=1862f4c3d3907e59b04a757cfda0ea7aa9ef39274af99a784f5be843c80c6772
