250ecbdba2d04b945d865e52c53ce99bbafab029
Ruurd Keizer <ruurdk@hotmail.com>
2018-01-08 14:40:00 +0100

    add goland arm binaries

--- ci/docker/main-ruby-go/install-go.sh
+++ ci/docker/main-ruby-go/install-go.sh
@@ -3,8 +3,10 @@
 set -eux
 # TODO ADD THE ARM Go Binaries or build from source
 GOPATH=/home/vagrant/go
-GO_ARCHIVE_URL=https://storage.googleapis.com/golang/go1.8.3.linux-amd64.tar.gz
-GO_ARCHIVE_SHA256=1862f4c3d3907e59b04a757cfda0ea7aa9ef39274af99a784f5be843c80c6772
+#GO_ARCHIVE_URL=https://storage.googleapis.com/golang/go1.8.3.linux-amd64.tar.gz
+#GO_ARCHIVE_SHA256=1862f4c3d3907e59b04a757cfda0ea7aa9ef39274af99a784f5be843c80c6772
+GO_ARCHIVE_URL=https://storage.googleapis.com/golang/go1.8.2.linux-armv6l.tar.gz
+GO_ARCHIVE_SHA256=a1942b2833e7d2685d7dbb7ac81c66125c351f24c7f006e8ae4a4283905257d1
 GO_ARCHIVE=/tmp/$(basename $GO_ARCHIVE_URL)
 
 echo "Downloading go..."
